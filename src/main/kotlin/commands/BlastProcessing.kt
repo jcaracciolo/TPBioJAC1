88dede950ca82e28162b7b517cfd79132d8ee791
Juan Franco <juan.caracciolo@wolox.com.ar>
2019-05-21 21:39:53 -0300

    Add local blast

--- src/main/kotlin/commands/BlastProcessing.kt
+++ src/main/kotlin/commands/BlastProcessing.kt
@@ -1,41 +1,64 @@
 package commands
 
 import org.biojava.nbio.core.sequence.DNASequence
-import org.biojava.nbio.core.sequence.MultipleSequenceAlignment
 import org.biojava.nbio.core.sequence.ProteinSequence
 import org.biojava.nbio.core.sequence.compound.*
 import org.biojava.nbio.core.sequence.io.*
-import org.biojava.nbio.core.sequence.io.template.SequenceHeaderParserInterface
-import org.biojava.nbio.ws.alignment.RemotePairwiseAlignmentProperties
+import org.biojava.nbio.core.sequence.template.AbstractCompound
+import org.biojava.nbio.core.sequence.template.AbstractSequence
 import org.biojava.nbio.ws.alignment.qblast.*
 import java.io.File
 import java.lang.Exception
+import java.io.InputStreamReader
+import java.io.BufferedReader
+
+
 
 class BlastProcessing(args: List<String>): CommandType {
-    val inputFile: File
-    val outputFile: File
-    val blastType: BlastType
+    private val inputFile: File
+    private val outputFile: File
+    private val remoteRequest: Boolean
+    private val blastType: BlastType
+    private val localBlastDB: String
 
-    enum class BlastType(val program: BlastProgramEnum, val db: String) {
-        AMINOACID(BlastProgramEnum.blastp, "swissprot"),
-        DNA(BlastProgramEnum.blastn, "nr");
+    enum class BlastType(val program: BlastProgramEnum, val programName: String,  val db: String) {
+        AMINOACID(BlastProgramEnum.blastp,"blastp", "swissprot"),
+        DNA(BlastProgramEnum.blastn, "blastn","nr");
     }
 
 
     init {
-        if( args.size != 2)
+        if( args.size < 2)
             throw InvalidCommandArgument("Blast processing recieves at least two arguments")
 
-        if(args.size >= 3) {
-            if(args[3].toLowerCase() == "dna") {
-                blastType = BlastType.DNA
-            } else if (args[3].toLowerCase() == "aminoacid") {
-                blastType = BlastType.AMINOACID
+        remoteRequest = if(args.size >=3) {
+            when(args[2].toLowerCase()) {
+                "local", "l" -> false
+                "remote", "r" -> true
+                else -> throw InvalidCommandArgument("${args[2]} is not a valid location type (r|remote|l|local)")
+            }
+        } else { true }
+
+        localBlastDB = if(!remoteRequest) {
+            if(args.size <=3) {
+                throw InvalidCommandArgument("Please specify a local DB (inputFile, outputFile, (l|local), localDB)")
             } else {
-                throw InvalidCommandArgument("${args[3]} is not a valid blast type (dna or aminoacid)")
+                args[3]
             }
         } else {
-            blastType = BlastType.AMINOACID
+            ""
+        }
+
+        val blastTypeIndex = if(remoteRequest) 4 else 5
+
+        blastType = if(args.size >= blastTypeIndex) {
+            when (args[blastTypeIndex - 1].toLowerCase()) {
+                "dna" -> BlastType.DNA
+                "aminoacid" -> BlastType.AMINOACID
+                else -> throw InvalidCommandArgument("${args[blastTypeIndex - 1]} is not a valid blast type (dna or aminoacid)")
+            }
+        } else {
+            BlastType.AMINOACID
         }
 
         inputFile = File(args[0])
@@ -50,7 +73,7 @@ class BlastProcessing(args: List<String>): CommandType {
 
     override fun execute() {
         //TODO Test DNA works
-        //TODO Make it so it works local
+        //TODO Test it works local
         println("Making request to Blas from file $inputFile saving to $outputFile")
 
         val proxy = if(blastType == BlastType.AMINOACID) {
@@ -67,12 +90,20 @@ class BlastProcessing(args: List<String>): CommandType {
             )
         }
 
-        val dnaSequences = proxy.process()
+        val dnaSequences = proxy.process().entries.map { it.key to it.value }
 
-        dnaSequences.entries.forEach { e ->
-            val name = e.key
-            val seq = e.value
 
+        if(remoteRequest) {
+            executeRemote(dnaSequences)
+        } else {
+            executeLocal(dnaSequences)
+        }
+
+        outputFile.delete()
+    }
+
+    private fun executeRemote(sequences: List<Pair<String, AbstractSequence<out AbstractCompound>>>) {
+        sequences.forEach { (name, seq) ->
             try {
                 val blast = NCBIQBlastService()
                 val properties = NCBIQBlastAlignmentProperties()
@@ -93,10 +124,34 @@ class BlastProcessing(args: List<String>): CommandType {
 
                 println("Done for $name..")
             }catch (e: Exception) {
-                println("Failed to retrieve Blast for $name")
+                println("Failed to retrieve Blast for $name - ${e.message}")
             }
+        }
+    }
 
-            outputFile.delete()
+    private fun executeLocal(sequences: List<Pair<String, AbstractSequence<out AbstractCompound>>>) {
+        sequences.forEach { (name, seq) ->
+            try {
+                val pb = ProcessBuilder(
+                    localBlastDB,
+                    "-db", blastType.db,
+                    "-outfmt", "5",
+                    "-query", inputFile.absolutePath,
+                    "-out", File(outputFile.path + "-$name").absolutePath
+                )
+
+                val p = pb.start()
+
+                val stdError = BufferedReader(InputStreamReader(p.errorStream))
+
+                stdError.lines().forEach {
+                    println("Error when blasting $name locally - $it")
+                }
+
+                println("Done for $name..")
+            }catch (e: Exception) {
+                println("Failed to retrieve Blast for $name - ${e.message}")
+            }
         }
     }
 
